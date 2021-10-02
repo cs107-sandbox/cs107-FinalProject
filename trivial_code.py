80ae97361c507b8418caf67263deed2cba0daaa7
ghcsung <gsung@g.harvard.edu>
2021-10-02 15:18:41 -0400

    codecov2

--- trivial_code.py
+++ trivial_code.py
@@ -5,11 +5,14 @@ Created on Sat Oct  2 13:48:00 2021
 @author: GSung
 """
 
+import unittest
 
-def test_sum():
-    assert sum([1, 2, 3]) == 6, "Should be 6"
-    
-    
-if __name__ == "__main__":
-    test_sum()
-    print("Everything passed")
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+
+if __name__ == '__main__':
+    unittest.main()
