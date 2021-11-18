e022ed2d6ecd1d13d47c1da778476b8914a19d67
Ivonne Martinez <ivonneam0925@gmail.com>
2021-11-18 11:23:30 -0500

    setup edit

--- setup.py
+++ setup.py
@@ -4,15 +4,18 @@ setuptools.setup(
     name = 'AutomaticDifferentiation',
     version = '0.0.1',
     author = 'Haoming Chen, Yiting Han, Ivonne Martinez, Gahyun Sung',
-    author_email = '',
+    author_email = 'ivonne_martinez@g.harvard.edu',
     description = 'Function Automatic Automatic Differentiation'
     long_description=open('docs/milestone1/README.md').read()
     long_description_content_type="text/markdown",
-    python_requires='>=3.6',
-    url = 'https://github.com/cs107-sandbox/cs107-FinalProject',
+    url = '',
+    install_requires = [
+        'some-pkg @ git+ssh://git@github.com/cs107-sandbox/cs107-FinalProject',
+        ]
     install_requires=['numpy', 'pytest', 'pip'],
     license ='MIT',
-    packages=['src'],
-    install_requires = ['numpy']
+    package_dir = {"":"src"},
+    packages = setuptools.find_packages(where='src'),
+    python_requires=">3.8",
 
 )
